aad8c11fdcd6722ddfc349874879122f42344f34
Namnextx <phuongnam.nextx@gmail.com>
2019-01-30 10:46:47 +0700

    1. Done

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
  * Created by nhatnk on 4/26/17.
  */
 
-function Hero(image, top, left, size){
+function Hero(image, top, left, size, speed){
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed=speed;
 
   this.getHeroElement = function(){
     return '<img width="'+ this.size + '"' +
@@ -16,20 +17,39 @@ function Hero(image, top, left, size){
   }
 
   this.moveRight = function(){
-    this.left += 20;
+    this.left += this.speed;
     console.log('ok: ' + this.left);
   }
-
-}
-
-var hero = new Hero('pikachu.png', 20, 30, 200);
-
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
+  this.moveLeft=function(){
+    this.left-=this.speed;
+    console.log('Ok'+ this.left);
   }
-  document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
-}
 
-start();
+}
+//
+var hero = new Hero('5628.png_1200.png', 20, 100, 100,100);
+var flag=true;
+function moveright(){
+  if(hero.left < window.innerWidth - hero.size && flag==true){
+    hero.moveRight();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    if (hero.left==1400) {
+      flag=false;
+    }
+  }
+  else if (hero.left>=window.innerWidth-hero.size) {
+    setTimeout(moveLeft1, 200);}
+   setTimeout(moveright, 200);
+}
+function moveLeft1() {
+  if(hero.left > 10 && flag==false){
+    hero.moveLeft();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+     if (hero.left==0){
+       flag=true;
+     }
+  }
+   setTimeout(moveLeft1, 200);
+
+}
+moveright();
